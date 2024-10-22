f88b3b829bb9461c0ff27f1f79edbbb27865a371
설승환 <tjftmd7@gmail.com>
2024-10-22 22:34:59 +0900

    feat: Treatment 엔티티 추가

---
+++ src/main/java/com/medicall/treatment/entity/Treatment.java
@@ -0,0 +1,33 @@
+package com.medicall.treatment.entity;
+
+import com.medicall.common.domain.BaseEntity;
+import com.medicall.member.domain.entity.Doctor;
+import com.medicall.member.domain.entity.Patient;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Treatment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
+}
