9fd25ed841f3579154affb998f8a38db942d9ca4
설승환 <tjftmd7@gmail.com>
2024-11-25 14:20:26 +0900

    feat: error 코드 추가 출처: yellobook

---
+++ src/main/java/com/medicall/error/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.medicall.error.exception;
+
+import com.medicall.error.code.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
