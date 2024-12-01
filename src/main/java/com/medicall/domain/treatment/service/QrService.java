0a780deb9627360c97ce943fc753ecc8b97fd0aa
설승환 <tjftmd7@gmail.com>
2024-12-01 22:16:32 +0900

    feat: QR코드 생성 서비스 추가

--- src/main/java/com/medicall/domain/treatment/service/QrService.java
+++ src/main/java/com/medicall/domain/treatment/service/QrService.java
@@ -1,2 +1,24 @@
-package com.medicall.domain.treatment.service;public class QrService {
+package com.medicall.domain.treatment.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.io.ByteArrayOutputStream;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QrService {
+    /*
+    qrcode 생성
+     */
+    public byte[] generateQrCode(String content, int width, int height) throws Exception{
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, width, height);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "png", outputStream);
+
+        return outputStream.toByteArray();
+    }
 }
